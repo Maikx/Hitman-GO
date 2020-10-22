f381567c5dcf93900cbf923ce0474dce73f80160
Maikx <mikeybee2000@gmail.com>
2020-10-22 11:26:11 +0200

    Rework with waypoints scripts

---
+++ Hitman GO/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Transactions;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public bool isMoving;
+    private Waypoints wpS;
+
+
+    void Start()
+    {
+        wpS = GameObject.Find("Level").GetComponent<Waypoints>();
+        isMoving = false;
+    }
+
+    void Update()
+    {
+        MoveToWayPoint();
+    }
+
+    void MoveToWayPoint()
+    {
+        if (transform.position != wpS.waypoint[wpS.current].position)
+        {
+            Vector3 pos = Vector3.MoveTowards(transform.position, wpS.waypoint[wpS.current].position, speed * Time.deltaTime);
+            GetComponent<Rigidbody>().MovePosition(pos);
+            isMoving = true;
+        }
+        else
+        {
+            isMoving = false;
+        }
+        if (Input.GetKeyDown(KeyCode.D) && isMoving == false)
+        {
+            wpS.current = (wpS.current + 1); //% waypoint.Length;
+        }
+    }
+}
---
+++ Hitman GO/Assets/Scripts/Waypoints.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoints : MonoBehaviour
+{
+    public Transform[] waypoint;
+    public int current = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
