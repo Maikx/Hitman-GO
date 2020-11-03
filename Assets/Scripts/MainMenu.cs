0d52040d493e095e94e3cc1819721979ee543ed1
Daniele Comotti <comotti.daniele@gmail.com>
2020-11-03 11:17:23 +0100

    Level selection scene added

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,8 +6,7 @@ using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    public Button[] LevelX;
-    public bool[] LevelXCom;
+    
     public GameObject LevelSelection;
     public GameObject Options;
     public GameObject Main;
@@ -24,83 +23,9 @@ public class MainMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(LevelXCom[0]==true)
-        {
-            LevelX[0].gameObject.SetActive(true);
-        }
-        else
-        {
-            LevelX[0].gameObject.SetActive(false);
-        }
-
-        if (LevelXCom[1] == true)
-        {
-            LevelX[1].gameObject.SetActive(true);
-        }
-        else
-        {
-            LevelX[1].gameObject.SetActive(false);
-        }
-
-        if (LevelXCom[2] == true)
-        {
-            LevelX[2].gameObject.SetActive(true);
-        }
-        else
-        {
-            LevelX[2].gameObject.SetActive(false);
-        }
-
-        if (LevelXCom[3] == true)
-        {
-            LevelX[3].gameObject.SetActive(true);
-        }
-        else
-        {
-            LevelX[3].gameObject.SetActive(false);
-        }
-
-        if (LevelXCom[4] == true)
-        {
-            LevelX[4].gameObject.SetActive(true);
-        }
-        else
-        {
-            LevelX[4].gameObject.SetActive(false);
-        }
+        
     }
 
-    #region Level Selection
-    public void GoToLevel1()
-    {
-        SceneManager.LoadScene(1);
-    }
-
-    public void GoToLevel2()
-    {
-        SceneManager.LoadScene(2);
-    }
-
-    public void GoToLevel3()
-    {
-        SceneManager.LoadScene(3);
-    }
-
-    public void GoToLevel4()
-    {
-        SceneManager.LoadScene(4);
-    }
-
-    public void GoToLevel5()
-    {
-        SceneManager.LoadScene(5);
-    }
-
-    public void GoToLevel6()
-    {
-        SceneManager.LoadScene(6);
-    }
-    #endregion
 
     public void GoToOptions()
     {
@@ -109,8 +34,7 @@ public class MainMenu : MonoBehaviour
     }
     public void GoToLevelSelection()
     {
-        Main.gameObject.SetActive(false);
-        LevelSelection.gameObject.SetActive(true);
+        SceneManager.LoadScene(1);
     }
     #region Options
     public void BackToMenu()
