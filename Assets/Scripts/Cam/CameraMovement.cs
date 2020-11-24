95ed4b82611c7a625f1bd1b32784ca6f5a7bd961
jacopo <jacopoice97@gmail.com>
2020-11-24 15:28:37 +0100

    Input Camera

--- Assets/Scripts/Cam/CameraMovement.cs
+++ Assets/Scripts/Cam/CameraMovement.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform parent;
+    public float speed = 10f;
+    public float rotation;
+    public float rotationY;
+    public bool isPressed = false;
+
+    public Transform currentPos;
+    private GameManager gm;
+    public Vector3 startPos;
+
+   public Vector3 nextPosition;
+   public float moveSpeed;
+
+    private void Start()
     {
-        
+       // nextPosition = transform.position;
+
     }
 
-    // Update is called once per frame
-    void Update()
+    void Movement()
     {
-        
+        if (isPressed == true)
+        {
+
+            
+            float x = Input.GetAxis("Mouse X") * speed * Time.deltaTime;
+            float y = Input.GetAxis("Mouse Y") * speed * Time.deltaTime;
+
+            rotation -= y;
+            rotationY += x;
+            rotation = Mathf.Clamp(rotation, -15f, 50f); //vertical
+            rotationY = Mathf.Clamp(rotationY, -30f, 30f);  //horizontal
+            parent.localRotation = Quaternion.Euler(0, rotationY,rotation);
+           
+
+ 
+        }
+
+        if (isPressed == false)
+        {
+        }
+
     }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0) || Input.GetMouseButton(1)) isPressed = true; else isPressed = false;
+        Movement();
+    }
+
 }
+
